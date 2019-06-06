57fd71802e97a3793783a9cd283228d2d5b80d24
karthikv21 <s531510@nwmissouri.edu>
2019-06-06 16:14:53 -0500

    Add Existing File

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { RouterModule } from '@angular/router';
+import { allAppRoutes } from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(allAppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
